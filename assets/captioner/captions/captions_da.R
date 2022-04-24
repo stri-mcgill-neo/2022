2d5fff363e86f7851567f812065a7bb099cf7ce5
jarrodscott <jarrod.jude.scott@gmail.com>
2022-04-24 14:24:57 -0500

    re-initialize gh-pages build

---
+++ assets/captioner/captions/captions_da.R
@@ -0,0 +1,149 @@
+##-##-##-##-##-##-##-##-##-##-##-##
+##-##-##-Table-captions-##-##-##-##
+##-##-##-##-##-##-##-##-##-##-##-##
+##################################
+############ 16S rRNA ############
+##################################
+
+####################################################################################################
+################################ IndVal Analysis ###################################################
+####################################################################################################
+caption_tab_ssu("ssu_inval_sum_tab", "Summary results of Indicator Analysis for each data set.")
+caption_tab_ssu("ssu_isa_full_Y0", "(Year 0) Significant results of Indicator Analysis for the FULL data set.")
+caption_tab_ssu("ssu_isa_full_Y1", "(Year 1) Significant results of Indicator Analysis for the FULL data set.")
+caption_tab_ssu("ssu_isa_full_Y4", "(Year 4) Significant results of Indicator Analysis for the FULL data set.")
+caption_tab_ssu("ssu_isa_filt_Y0", "(Year 0) Significant results of Indicator Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_ssu("ssu_isa_filt_Y1", "(Year 1) Significant results of Indicator Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_ssu("ssu_isa_filt_Y4", "(Year 4) Significant results of Indicator Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_ssu("ssu_isa_perfect_Y0", "(Year 0) Significant results of Indicator Analysis for the PERfect filtered data set.")
+caption_tab_ssu("ssu_isa_perfect_Y1", "(Year 1) Significant results of Indicator Analysis for the PERfect filtered data set.")
+caption_tab_ssu("ssu_isa_perfect_Y4", "(Year 4) Significant results of Indicator Analysis for the PERfect filtered data set.")
+caption_tab_ssu("ssu_isa_pime_Y0", "(Year 0) Significant results of Indicator Analysis for the PIME filtered data set.")
+caption_tab_ssu("ssu_isa_pime_Y1", "(Year 1) Significant results of Indicator Analysis for the PIME filtered data set.")
+caption_tab_ssu("ssu_isa_pime_Y4", "(Year 4) Significant results of Indicator Analysis for the PIME filtered data set.")
+
+caption_tab_ssu("ssu_ps_work_isa_combo", "(FULL data set) Combined results of IndVal analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_ssu("ssu_ps_filt_isa_combo", "(Arbitrary filtered) Combined results of IndVal analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_ssu("ssu_ps_perfect_isa_combo", "(PERFect filtered) Combined results of IndVal analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_ssu("ssu_ps_pime_isa_combo", "(PIME filtered) Combined results of IndVal analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+####################################################################################################
+################################ LEFse ASV       ###################################################
+####################################################################################################
+caption_tab_ssu("ssu_lefse_sum_tab", "Summary results of LEfSe Analysis for each data set.")
+caption_tab_ssu("ssu_lefse_asv_work_Y0", "(Year 0) Significant results of the LEfSe ASV Analysis for the FULL data set.")
+caption_tab_ssu("ssu_lefse_asv_work_Y1", "(Year 1) Significant results of the LEfSe ASV Analysis for the FULL data set.")
+caption_tab_ssu("ssu_lefse_asv_work_Y4", "(Year 4) Significant results of the LEfSe ASV Analysis for the FULL data set.")
+caption_tab_ssu("ssu_lefse_asv_filt_Y0", "(Year 0) Significant results of the LEfSe ASV Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_ssu("ssu_lefse_asv_filt_Y1", "(Year 1) Significant results of the LEfSe ASV Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_ssu("ssu_lefse_asv_filt_Y4", "(Year 4) Significant results of the LEfSe ASV Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_ssu("ssu_lefse_asv_perfect_Y0", "(Year 0) Significant results of the LEfSe ASV Analysis for the PERfect filtered data set.")
+caption_tab_ssu("ssu_lefse_asv_perfect_Y1", "(Year 1) Significant results of the LEfSe ASV Analysis for the PERfect filtered data set.")
+caption_tab_ssu("ssu_lefse_asv_perfect_Y4", "(Year 4) Significant results of the LEfSe ASV Analysis for the PERfect filtered data set.")
+caption_tab_ssu("ssu_lefse_asv_pime_Y0", "(Year 0) Significant results of the LEfSe ASV Analysis for the PIME filtered data set.")
+caption_tab_ssu("ssu_lefse_asv_pime_Y1", "(Year 1) Significant results of the LEfSe ASV Analysis for the PIME filtered data set.")
+caption_tab_ssu("ssu_lefse_asv_pime_Y4", "(Year 4) Significant results of the LEfSe ASV Analysis for the PIME filtered data set.")
+
+caption_tab_ssu("ssu_ps_work_lefse_combo", "(FULL data set) Combined results of LEfSe analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_ssu("ssu_ps_filt_lefse_combo", "(Arbitrary filtered) Combined results of LEfSe analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_ssu("ssu_ps_perfect_lefse_combo", "(PERFect filtered) Combined results of LEfSe analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_ssu("ssu_ps_pime_lefse_combo", "(PIME filtered) Combined results of LEfSe analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+
+####################################################################################################
+################################ LEFse Marker    ###################################################
+####################################################################################################
+caption_tab_ssu("ssu_lefse_marker_work_Y0", "(Year 0) Significant results of the LEfSe Marker Analysis for the FULL data set.")
+caption_tab_ssu("ssu_lefse_marker_work_Y1", "(Year 1) Significant results of the LEfSe Marker Analysis for the FULL data set.")
+caption_tab_ssu("ssu_lefse_marker_work_Y4", "(Year 4) Significant results of the LEfSe Marker Analysis for the FULL data set.")
+caption_tab_ssu("ssu_lefse_marker_filt_Y0", "(Year 0) Significant results of the LEfSe Marker Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_ssu("ssu_lefse_marker_filt_Y1", "(Year 1) Significant results of the LEfSe Marker Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_ssu("ssu_lefse_marker_filt_Y4", "(Year 4) Significant results of the LEfSe Marker Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_ssu("ssu_lefse_marker_perfect_Y0", "(Year 0) Significant results of the LEfSe Marker Analysis for the PERfect filtered data set.")
+caption_tab_ssu("ssu_lefse_marker_perfect_Y1", "(Year 1) Significant results of the LEfSe Marker Analysis for the PERfect filtered data set.")
+caption_tab_ssu("ssu_lefse_marker_perfect_Y4", "(Year 4) Significant results of the LEfSe Marker Analysis for the PERfect filtered data set.")
+caption_tab_ssu("ssu_lefse_marker_pime_Y0", "(Year 0) Significant results of the LEfSe Marker Analysis for the PIME filtered data set.")
+caption_tab_ssu("ssu_lefse_marker_pime_Y1", "(Year 1) Significant results of the LEfSe Marker Analysis for the PIME filtered data set.")
+caption_tab_ssu("ssu_lefse_marker_pime_Y4", "(Year 4) Significant results of the LEfSe Marker Analysis for the PIME filtered data set.")
+
+##################################
+############ ITS #################
+##################################
+
+####################################################################################################
+################################ IndVal Analysis ###################################################
+####################################################################################################
+caption_tab_its("its_inval_sum_tab", "Summary results of Indicator Analysis for each data set.")
+caption_tab_its("its_isa_full_Y0", "(Year 0) Significant results of Indicator Analysis for the FULL data set.")
+caption_tab_its("its_isa_full_Y1", "(Year 1) Significant results of Indicator Analysis for the FULL data set.")
+caption_tab_its("its_isa_full_Y4", "(Year 4) Significant results of Indicator Analysis for the FULL data set.")
+caption_tab_its("its_isa_filt_Y0", "(Year 0) Significant results of Indicator Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_its("its_isa_filt_Y1", "(Year 1) Significant results of Indicator Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_its("its_isa_filt_Y4", "(Year 4) Significant results of Indicator Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_its("its_isa_perfect_Y0", "(Year 0) Significant results of Indicator Analysis for the PERfect filtered data set.")
+caption_tab_its("its_isa_perfect_Y1", "(Year 1) Significant results of Indicator Analysis for the PERfect filtered data set.")
+caption_tab_its("its_isa_perfect_Y4", "(Year 4) Significant results of Indicator Analysis for the PERfect filtered data set.")
+caption_tab_its("its_isa_pime_Y0", "(Year 0) Significant results of Indicator Analysis for the PIME filtered data set.")
+caption_tab_its("its_isa_pime_Y1", "(Year 1) Significant results of Indicator Analysis for the PIME filtered data set.")
+caption_tab_its("its_isa_pime_Y4", "(Year 4) Significant results of Indicator Analysis for the PIME filtered data set.")
+
+caption_tab_its("its_ps_work_isa_combo", "(FULL data set) Combined results of IndVal analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_its("its_ps_filt_isa_combo", "(Arbitrary filtered) Combined results of IndVal analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_its("its_ps_perfect_isa_combo", "(PERFect filtered) Combined results of IndVal analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_its("its_ps_pime_isa_combo", "(PIME filtered) Combined results of IndVal analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+####################################################################################################
+################################ LEFse ASV       ###################################################
+####################################################################################################
+caption_tab_its("its_lefse_sum_tab", "Summary results of LEfSe Analysis for each data set.")
+caption_tab_its("its_lefse_asv_work_Y0", "(Year 0) Significant results of the LEfSe ASV Analysis for the FULL data set.")
+caption_tab_its("its_lefse_asv_work_Y1", "(Year 1) Significant results of the LEfSe ASV Analysis for the FULL data set.")
+caption_tab_its("its_lefse_asv_work_Y4", "(Year 4) Significant results of the LEfSe ASV Analysis for the FULL data set.")
+caption_tab_its("its_lefse_asv_filt_Y0", "(Year 0) Significant results of the LEfSe ASV Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_its("its_lefse_asv_filt_Y1", "(Year 1) Significant results of the LEfSe ASV Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_its("its_lefse_asv_filt_Y4", "(Year 4) Significant results of the LEfSe ASV Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_its("its_lefse_asv_perfect_Y0", "(Year 0) Significant results of the LEfSe ASV Analysis for the PERfect filtered data set.")
+caption_tab_its("its_lefse_asv_perfect_Y1", "(Year 1) Significant results of the LEfSe ASV Analysis for the PERfect filtered data set.")
+caption_tab_its("its_lefse_asv_perfect_Y4", "(Year 4) Significant results of the LEfSe ASV Analysis for the PERfect filtered data set.")
+caption_tab_its("its_lefse_asv_pime_Y0", "(Year 0) Significant results of the LEfSe ASV Analysis for the PIME filtered data set.")
+caption_tab_its("its_lefse_asv_pime_Y1", "(Year 1) Significant results of the LEfSe ASV Analysis for the PIME filtered data set.")
+caption_tab_its("its_lefse_asv_pime_Y4", "(Year 4) Significant results of the LEfSe ASV Analysis for the PIME filtered data set.")
+
+caption_tab_its("its_ps_work_lefse_combo", "(FULL data set) Combined results of LEfSe analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_its("its_ps_filt_lefse_combo", "(Arbitrary filtered) Combined results of LEfSe analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_its("its_ps_perfect_lefse_combo", "(PERFect filtered) Combined results of LEfSe analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+caption_tab_its("its_ps_pime_lefse_combo", "(PIME filtered) Combined results of LEfSe analysis for each year showing which year and treatment an ASV was enriched in. Please note that NA for total reads and frequency DOES NOT indicate the ASV was absent for a given year, just that it was not significant.")
+
+####################################################################################################
+################################ LEFse Marker    ###################################################
+####################################################################################################
+caption_tab_its("its_lefse_marker_work_Y0", "(Year 0) Significant results of the LEfSe Marker Analysis for the FULL data set.")
+caption_tab_its("its_lefse_marker_work_Y1", "(Year 1) Significant results of the LEfSe Marker Analysis for the FULL data set.")
+caption_tab_its("its_lefse_marker_work_Y4", "(Year 4) Significant results of the LEfSe Marker Analysis for the FULL data set.")
+caption_tab_its("its_lefse_marker_filt_Y0", "(Year 0) Significant results of the LEfSe Marker Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_its("its_lefse_marker_filt_Y1", "(Year 1) Significant results of the LEfSe Marker Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_its("its_lefse_marker_filt_Y4", "(Year 4) Significant results of the LEfSe Marker Analysis for the Arbitrary filtered ASV data set.")
+caption_tab_its("its_lefse_marker_perfect_Y0", "(Year 0) Significant results of the LEfSe Marker Analysis for the PERfect filtered data set.")
+caption_tab_its("its_lefse_marker_perfect_Y1", "(Year 1) Significant results of the LEfSe Marker Analysis for the PERfect filtered data set.")
+caption_tab_its("its_lefse_marker_perfect_Y4", "(Year 4) Significant results of the LEfSe Marker Analysis for the PERfect filtered data set.")
+caption_tab_its("its_lefse_marker_pime_Y0", "(Year 0) Significant results of the LEfSe Marker Analysis for the PIME filtered data set.")
+caption_tab_its("its_lefse_marker_pime_Y1", "(Year 1) Significant results of the LEfSe Marker Analysis for the PIME filtered data set.")
+caption_tab_its("its_lefse_marker_pime_Y4", "(Year 4) Significant results of the LEfSe Marker Analysis for the PIME filtered data set.")
+
+
+#<small>`r caption_tab_ssu("qual_scores_after_ssu")`</small>
+##-##-##-##-##-##-##-##-##-##-##-##-##-##
+##-##-##    Figure captions   ##-##-##-##
+##-##-##-##-##-##-##-##-##-##-##-##-##-##
+
+caption_fig_ssu("ssu_lefse_asv_full", "Differentially abundant ASVs from LEfSe analysis for the FULL data set, trimmed to show those with LDA score \u2265 3.0.")
+caption_fig_ssu("ssu_lefse_asv_filt", "Differentially abundant ASVs from LEfSe analysis for the Arbitrary filtered data set, trimmed to show those with LDA score \u2265 3.0.")
+caption_fig_ssu("ssu_lefse_asv_perfect", "Differentially abundant ASVs from LEfSe analysis for the PERfect filtered data set, trimmed to show those with LDA score \u2265 3.0.")
+caption_fig_ssu("ssu_lefse_asv_pime", "Differentially abundant ASVs from LEfSe analysis for the PIME filtered data set, trimmed to show those with LDA score \u2265 3.0.")
+
+caption_fig_ssu("ssu_lefse_marker_full", "Differentially abundant MARKER from LEfSe analysis for the FULL data set, trimmed to show those with LDA score \u2265 4.0.")
+caption_fig_ssu("ssu_lefse_marker_filt", "Differentially abundant MARKER from LEfSe analysis for the Arbitrary filtered data set, trimmed to show those with LDA score \u2265 4.0.")
+caption_fig_ssu("ssu_lefse_marker_perfect", "Differentially abundant MARKER from LEfSe analysis for the PERfect filtered data set, trimmed to show those with LDA score \u2265 4.0.")
+caption_fig_ssu("ssu_lefse_marker_pime", "Differentially abundant MARKER from LEfSe analysis for the PIME filtered data set, trimmed to show those with LDA score \u2265 4.0.")
+
+
+#<small>`r caption_fig_ssu("ssu_lefse_asv_full")`</small>
+
+
